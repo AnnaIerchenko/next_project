847bfe5394083d9f6f552ccde25ff4ecfff6664a
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2024-12-15 08:53:52 -0800

    add search input at header, add popup with list  by searchInput

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -4,6 +4,8 @@ import { Container } from './container';
 import Image from 'next/image';
 import { Button } from '../ui';
 import { ArrowRight, ShoppingCart, User } from 'lucide-react';
+import Link from 'next/link';
+import { SearchInput } from '.';
 
 
 interface Props {
@@ -15,12 +17,18 @@ export const Header: React.FC<Props> = ({className}) => {
     <header className={cn('border-border-b',className)}>
       <Container className='flex items-center justify-between py-8'>
         {/* left side */}
-        <div className='flex items-center gap-4'>
+        <Link href="/">    
+          <div className='flex items-center gap-4'>
           <Image src="/logo.png" alt="logo" width={35} height={35}/>
           <div>
             <h1 className='text-2xl uppercase font-bold'>Next Food</h1>
             <p className='text-sm text-gray-400 leading-3'>the best taste</p>
           </div>
+          </div>
+        </Link>
+
+        <div className='mx-10 flex-1'>
+          <SearchInput />
         </div>
         {/* right side */}
         <div className='flex items-center gap-4'>
