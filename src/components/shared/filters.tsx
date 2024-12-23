0aaee146e4e520a41b8a9c98ef283d0ee5422a37
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2024-12-22 23:53:32 -0800

    aside slider filter

--- src/components/shared/filters.tsx
+++ src/components/shared/filters.tsx
@@ -9,11 +9,21 @@ import { useFilterIngredients } from "../../../hooks/useFilterIngredients"
 interface Props {
   className?: string
 }
-
+interface PriceProps {
+  priceFrom: number;
+  priceTo: number;
+}
 export const Filters: React.FC<Props> = ({className}) => {
   const {ingredients, loading, onAddId, selectedIds} = useFilterIngredients()
+  const [prices, setPrice] = React.useState<PriceProps>({ priceFrom:0, priceTo: 1000})
   const items = ingredients.map((item) => ({ value: String(item.id), text: item.name}))
 
+  const updatePrice = (name: keyof PriceProps, value: number) => {
+    setPrice({
+      ...prices,
+      [name]: value,
+    })
+  }
   return (
     <div className={className}>
       <Title text="Filter" size="sm" className="mb-5 font-bold"/>
@@ -26,10 +36,26 @@ export const Filters: React.FC<Props> = ({className}) => {
       <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
         <p className="font-bold mb-3">Price from ... to</p>
         <div className="flex gap-3 mb-5">
-          <Input type="number" placeholder="0" min={0} max={1000} defaultValue={0}/>
-          <Input type="number" min={100} max={1000} placeholder="30000"/>
+          <Input 
+            type="number" 
+            placeholder="0" 
+            min={0} max={1000} 
+            value={String(prices.priceFrom)} 
+            onChange={(e) => updatePrice('priceFrom', Number(e.target.value))}  
+          />
+          <Input 
+            type="number" 
+            min={100} 
+            max={1000} 
+            placeholder="30000" 
+            value={String(prices.priceTo)} 
+            onChange={(e) => updatePrice('priceTo', Number(e.target.value))}    
+          />
         </div>
-        <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
+        <RangeSlider min={0} max={1000} step={10} 
+          value={[prices.priceFrom, prices.priceTo]} 
+          onValueChange={([priceFrom, priceTo]) => setPrice({ priceFrom, priceTo })}
+        />
       </div>
 
       <CheckboxFiltersGroup 
