52e184801af11a69317c7209e704e5abe03d2a6f
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2024-12-24 09:02:20 -0800

    get products and navigation list from backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 import { Container, Filters, ProductsGroupList, Title, TopBar } from "@/components/shared";
+import { prisma } from "../../prisma/prisma-client";
 
 
-export default function Home() {
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          items: true,
+        }
+      }
+    }
+  })
   return (
     <>
       <Container className="mt-10">
         <Title text='All Pizzas' size='lg' className='font-extrabold'/>
       </Container> 
-      <TopBar />
+      <TopBar categories={categories.filter((category) => category.products.length > 0)} />
       <Container className="mt-10 pb-14">
         <div className="flex gap-[80px]">
           {/* filter */}
@@ -18,84 +29,15 @@ export default function Home() {
           {/* list of products*/}
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductsGroupList title="Pizzas" items={[
-                {
-                  id: 1,
-                  name: "Cheesburger Pizza",
-                  imageUrl: "https://media.dodostatic.net/image/r:233x233/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                  price: 10,
-                  items: [{ price: 15}],
-                },
-                {
-                  id: 2,
-                  name: "Cheesburger Pizza",
-                  imageUrl: "https://media.dodostatic.net/image/r:233x233/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                  price: 10,
-                  items: [{ price: 15}],
-                },
-                {
-                  id: 3,
-                  name: "Cheesburger Pizza",
-                  imageUrl: "https://media.dodostatic.net/image/r:233x233/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                  price: 10,
-                  items: [{ price: 15}],
-                },
-                {
-                  id: 4,
-                  name: "Cheesburger Pizza",
-                  imageUrl: "https://media.dodostatic.net/image/r:233x233/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                  price: 10,
-                  items: [{ price: 15}],
-                },
-                {
-                  id: 5,
-                  name: "Cheesburger Pizza",
-                  imageUrl: "https://media.dodostatic.net/image/r:233x233/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                  price: 10,
-                  items: [{ price: 15}],
-                },
-                ]} 
-                categoryId={1}
-              />
-              <ProductsGroupList title="Kombo" items={[
-                {
-                  id: 1,
-                  name: "Cheesburger Pizza",
-                  imageUrl: "https://media.dodostatic.net/image/r:233x233/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                  price: 10,
-                  items: [{ price: 15}],
-                },
-                {
-                  id: 2,
-                  name: "Cheesburger Pizza",
-                  imageUrl: "https://media.dodostatic.net/image/r:233x233/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                  price: 10,
-                  items: [{ price: 15}],
-                },
-                {
-                  id: 3,
-                  name: "Cheesburger Pizza",
-                  imageUrl: "https://media.dodostatic.net/image/r:233x233/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                  price: 10,
-                  items: [{ price: 15}],
-                },
-                {
-                  id: 4,
-                  name: "Cheesburger Pizza",
-                  imageUrl: "https://media.dodostatic.net/image/r:233x233/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                  price: 10,
-                  items: [{ price: 15}],
-                },
-                {
-                  id: 5,
-                  name: "Cheesburger Pizza",
-                  imageUrl: "https://media.dodostatic.net/image/r:233x233/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                  price: 10,
-                  items: [{ price: 15}],
-                },
-                ]} 
-                categoryId={2}
-              />
+              {categories.map(
+                (category) => category.products.length > 0 && (
+                  <ProductsGroupList 
+                    key={category.id}
+                    title={category.name}
+                    categoryId={category.id}
+                    items = {category.products}
+                  />
+                ))}
             </div>
           </div>
         </div>
--- src/components/shared/categories.tsx
+++ src/components/shared/categories.tsx
@@ -2,27 +2,29 @@
 import { cn } from '@/lib/utils'
 import React from 'react'
 import { useCategoryStore } from '../../../store/category'
+import { Category } from '@prisma/client';
 
 interface Props {
-  className?: string
+  items: Category[];
+  className?: string;
 }
-const categories = [
-  { id: 1, name: "Pizza"},
-  { id: 2, name: "Kombo"},
-  { id: 3, name: "Nibbles"},
-  { id: 4, name: "Cocktails"},
-  { id: 5, name: "Coffe"},
-  { id: 6, name: "Drinks"},
-  { id: 7, name: "Deserts"},
-]
+// const categories = [
+//   { id: 1, name: "Pizzas"},
+//   { id: 2, name: "Kombo"},
+//   { id: 3, name: "Nibbles"},
+//   { id: 4, name: "Cocktails"},
+//   { id: 5, name: "Coffe"},
+//   { id: 6, name: "Drinks"},
+//   { id: 7, name: "Deserts"},
+// ]
 
 
-export const Categories: React.FC<Props> = ({className}) => {
+export const Categories: React.FC<Props> = ({items, className}) => {
   const categoryActiveId = useCategoryStore((state) => state.activeId)
   return (
     <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl',className)}>
       {
-        categories.map(({name, id}, ind) => (
+        items.map(({name, id}, ind) => (
           <a 
             className={cn('flex items-center font-bold h-11 rounded-2xl px-5',
             categoryActiveId === id && "bg-white shadow-md shadow-gray-200 text-primary")}
