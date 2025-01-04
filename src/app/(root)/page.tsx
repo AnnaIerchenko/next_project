dd59a1393b4bd0034ab8462f5d5c437665aa8e28
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2025-01-04 11:56:17 -0800

    modal for choose product

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,5 @@
-import { Container, Filters, ProductsGroupList, Title, TopBar } from "@/components/shared";
-import { prisma } from "../../prisma/prisma-client";
-
+import { Container, Filters, ProductsGroupList, Title, TopBar } from '@/components/shared';
+import { prisma } from '../../../prisma/prisma-client';
 
 export default async function Home() {
   const categories = await prisma.category.findMany({
@@ -9,15 +8,15 @@ export default async function Home() {
         include: {
           ingredients: true,
           items: true,
-        }
-      }
-    }
-  })
+        },
+      },
+    },
+  });
   return (
     <>
       <Container className="mt-10">
-        <Title text='All Pizzas' size='lg' className='font-extrabold'/>
-      </Container> 
+        <Title text="All Pizzas" size="lg" className="font-extrabold" />
+      </Container>
       <TopBar categories={categories.filter((category) => category.products.length > 0)} />
       <Container className="mt-10 pb-14">
         <div className="flex gap-[80px]">
@@ -30,14 +29,16 @@ export default async function Home() {
           <div className="flex-1">
             <div className="flex flex-col gap-16">
               {categories.map(
-                (category) => category.products.length > 0 && (
-                  <ProductsGroupList 
-                    key={category.id}
-                    title={category.name}
-                    categoryId={category.id}
-                    items = {category.products}
-                  />
-                ))}
+                (category) =>
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      categoryId={category.id}
+                      items={category.products}
+                    />
+                  ),
+              )}
             </div>
           </div>
         </div>
--- src/app/(root)/product/[id]/page.tsx
+++ src/app/(root)/product/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from 'next/navigation';
-import { prisma } from '../../../../prisma/prisma-client';
+import { prisma } from '../../../../../prisma/prisma-client';
 import { Container, GroupVariants, ProductImage, Title } from '@/components/shared';
 
 export default async function ProductPage({ params: { id } }: { params: { id: string } }) {
